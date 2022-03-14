7617e1a26e1953635d0e9823f1e53e164ffafc8f
FPerkiss <95028634+FPerkiss@users.noreply.github.com>
2022-03-14 22:07:21 +0000

    Added property id

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ from time import sleep #Adding sleep (prevent DDOS)
 
 address =[]
 house_price = []
+property_id = []
+page_number = 400 # The url goes up by 24 each time.
 
-URL = "https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=REGION%5E61307&index={page_number}&propertyTypes=&includeSSTC=false&mustHave=&dontShow=&furnishTypes=&keywords="
+URL = f"https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=REGION%5E61307&index={page_number}&propertyTypes=&includeSSTC=false&mustHave=&dontShow=&furnishTypes=&keywords="
 page = r.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
-
-
-
 
 #Gives us a number of listings
 numberOfListings = soup.find("span", {"class": "searchHeader-resultCount"})
@@ -23,7 +22,6 @@ numberOfListings = int(numberOfListings.replace(",", ""))
 lastListing = 0
 
 if numberOfListings >= lastListing:
-
     property_search = soup.find(id="propertySearch")
     container = property_search.find(id="propertySearch-results-container")
     search_results = container.find(id="l-searchResults")
@@ -39,19 +37,29 @@ if numberOfListings >= lastListing:
         #print(address_element['content'])
         address.append(address_element['content'])
         house_price.append(price_element.text)
+        lastListing ++1
+
+##################################################
+        # ID_element = house_listing.find_all("prop")
+        #ID_element = house_listing.find('div', {'class': 'propertyCard propertyCard--featured propertyCard--fsw-updates'})
+        #ID_tag = soup.find('a')
+        #listing_ID = ID_tag.attrs['id']
+
+        #testing = listing_ID.get_text()
+        #testing = int(listing_ID.replace(",", ""))
+        #print(ID_element)
+
+
+#########################################################
+
 
 
 
     waitTime = random.uniform(1, 8)  # Random number between 1-8 seconds
     print(waitTime) #Waits for x seconds
-    page_number = +24 # The url goes up by 24 each time.
+    page_number = 48 # The url goes up by 24 each time.
     lastListing = lastListing+1 #Keeps track of property number
     sleep(waitTime) # Adds delay of between 1 - 8 seconds
-
-
-print(address)
-print(house_price)
-
 
 
 #Data export
@@ -62,5 +70,3 @@ data = {
 }
 df=pd.DataFrame.from_dict(data)
 df.to_csv(r"TestData.csv", encoding="utf-8")
-
-
